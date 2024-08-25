d4f50c6adf0552314bf853b887bdeeab526e26a8
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-25 15:30:56 -0300

    feat: validaçao do formulário

--- src/components/NetflixInput.jsx
+++ src/components/NetflixInput.jsx
@@ -1,47 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
+import { LiaTimesCircle } from "react-icons/lia";
 
-export function NetflixInput({ type, label, name }) {
-  const [isFocused, setIsFocused] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
-
-  const handleBlur = () => {
-    if (inputValue === "") {
-      setIsFocused(false);
-    }
-  };
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
+export function NetflixInput({ type, label, name, register, errorMessage }) {
   return (
-    <div className="relative w-full mt-4 border border-neutral-600 rounded">
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={inputValue}
-        onChange={handleChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        className={`bg-neutral-900/60 text-white block w-full px-4 pt-5 pb-2 text-lg outline-none transition-all${
-          isFocused || inputValue ? "border-blue-500" : "border-gray-400"
-        }`}
-      />
-      <label
-        htmlFor={name}
-        className={`absolute left-4 top-3 text-lg text-neutral-400 transition-all transform ${
-          isFocused || inputValue
-            ? "-translate-y-2 text-sm"
-            : "translate-y-0"
-        }`}
-      >
-        {label}
-      </label>
-    </div>
+    <fieldset>
+      <div className="relative w-full border border-neutral-600 rounded group focus-within:border-blue-200">
+        <input
+          type={type}
+          id={name}
+          name={name}
+          placeholder=" "
+          className={`bg-neutral-900/60 text-white block w-full px-4 pt-5 pb-2 text-lg outline-none transition-all peer/input`}
+          {...register}
+        />
+        <label
+          htmlFor={name}
+          className={`absolute left-4 top-3 text-sm text-neutral-400 transition-all transform  peer-placeholder-shown/input:translate-y-0 group-focus-within:!-translate-y-2 -translate-y-2 
+            peer-placeholder-shown/input:text-lg group-focus-within:!text-sm
+            `}
+        >
+          {label}
+        </label>
+      </div>
+      {errorMessage && (
+        <p className="text-red-500 flex gap-1 pt-1">
+          {" "}
+          <LiaTimesCircle />
+          <span className="text-xs">{errorMessage}</span>
+        </p>
+      )}
+    </fieldset>
   );
 }
--- src/pages/login/components/FormLogin.jsx
+++ src/pages/login/components/FormLogin.jsx
@@ -1,20 +1,58 @@
+import { object, string } from "yup";
 import { NetflixInput } from "../../../components/NetflixInput";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+
+const userSchema = object({
+  email: string()
+    .test(
+      "is-email-or-phone",
+      "Informe um email ou número de telefone válido.",
+      (value) => emailRegex.test(value) || phoneRegex.test(value)
+    )
+    .required("Campo obrigatório"),
+  password: string()
+    .min(4, "A senha deve ter entre 4 e 60 caracteres.")
+    .max(60, "A senha deve ter entre 4 e 60 caracteres.")
+    .required("Campo obrigatório"),
+});
 
 export function FormLogin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="m-auto py-12 px-16 max-w-[28.125rem] bg-black/70 rounded z-20 relative">
       <h1 className="text-white text-3xl font-bold mb-7">Entrar</h1>
-      <form className="grid gap-4 text-center">
-        <fieldset>
-          <NetflixInput
-            type={"text"}
-            label={"Email ou número de celular"}
-            name={"text"}
-          />
-        </fieldset>
-        <fieldset>
-          <NetflixInput type={"password"} label={"Senha"} name={"password"} />
-        </fieldset>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid gap-4 text-center transition-all"
+      >
+        <NetflixInput
+          register={{ ...register("email") }}
+          errorMessage={errors.email?.message}
+          type={"text"}
+          label={"Email ou número de celular"}
+          name={"text"}
+        />
+
+        <NetflixInput
+          register={{ ...register("password") }}
+          errorMessage={errors.password?.message}
+          type={"password"}
+          label={"Senha"}
+          name={"password"}
+        />
 
         <button className="bg-red-600 p-2 rounded font-medium text-white hover:bg-red-700">
           Entrar
