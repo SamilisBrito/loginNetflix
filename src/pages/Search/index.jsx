71a7627fc271f3f5d9f8458a482cd79ce219f25c
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-27 23:32:20 -0300

    wip: adicionar busca  e renderizar componentes dos filmes

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,7 +1,35 @@
+import { useContext, useEffect } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { Context } from "../../components/Context";
+import { Movie } from "../../components/Movie";
+
 export function Search() {
-  return(
-    <div>
-      <h1>Pesquisa e tals</h1>
+  const { query } = useContext(Context);
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    getData();
+  }, [query]);
+
+  async function getData() {
+    if (query !== "")
+      await request(
+        `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${import.meta.env.VITE_APP_TMDB_API_TOKEN}`,
+          },
+        }
+      );
+  }
+  console.log(query, data);
+
+  return (
+    <div className="flex flex-wrap gap-5">
+      {data?.results.map((movie) => (
+        <Movie key={movie.id} movie={movie} />
+      ))}
     </div>
-  )
+  );
 }
