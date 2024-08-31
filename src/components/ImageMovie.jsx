12458e48ca62feef95cdb16e7ca0c059f7699362
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-31 20:40:01 -0300

    wip: generalizar imagem

--- src/components/ImageMovie.jsx
+++ src/components/ImageMovie.jsx
@@ -1,14 +1,20 @@
 import { useModalMovie } from "../hooks/useModalMovie";
+import defaultImage from "../assets/netflix.svg";
 
-export function Movie({ movie }) {
+export function Movie({ movie, width }) {
   const { onMovieClick } = useModalMovie();
 
+  console.log("movie", movie);
+
   return (
-    <div className="text-white mr-2 ">
+    <div className={`text-white mr-2 ${width} overflow-hidden`}>
       <img
-        className="w-full rounded"
-        src={`
-https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
+        className="w-full h-full object-cover rounded"
+        src={
+          movie?.backdrop_path
+            ? `https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`
+            : defaultImage
+        }
         alt={movie.title}
         onClick={() => onMovieClick(movie)}
       />
